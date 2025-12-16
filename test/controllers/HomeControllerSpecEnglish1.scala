4e2be3556346cd52f4a7856b5e9586cc36111b89
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-12-16 10:24:15 +0000

    https://github.com/toposoid/toposoid-easy-search-web/issues/36

--- test/controllers/HomeControllerSpecEnglish1.scala
+++ test/controllers/HomeControllerSpecEnglish1.scala
@@ -17,7 +17,7 @@
 
 package controllers
 
-import akka.util.Timeout
+import org.apache.pekko.util.Timeout
 import com.ideal.linked.common.DeploymentConverter.conf
 import com.ideal.linked.toposoid.common.{TRANSVERSAL_STATE, ToposoidUtils, TransversalState}
 import com.ideal.linked.toposoid.knowledgebase.regist.model.{ImageReference, Knowledge, KnowledgeForImage, PropositionRelation, Reference}
@@ -36,7 +36,7 @@ import play.api.http.Status.OK
 import play.api.libs.json.Json
 import play.api.test.Helpers.{POST, contentType, status, _}
 import play.api.test._
-import io.jvm.uuid.UUID
+//import io.jvm.uuid.UUID
 import scala.concurrent.duration.DurationInt
 
 class HomeControllerSpecEnglish1 extends PlaySpec with BeforeAndAfter with BeforeAndAfterAll with GuiceOneAppPerSuite with DefaultAwaitTimeout with Injecting {
